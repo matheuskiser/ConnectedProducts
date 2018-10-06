c56ab4d5970fc7aff791f6426cfad0b8e48fdac9
Matheus Iser <matheus.iser@nike.com>
2018-10-06 14:03:51 -0700

    Shows how to go from a fragment to a different activity

---
+++ app/src/main/java/com/nike/iser/connectedproducts/OutsideActivity.kt
@@ -0,0 +1,12 @@
+package com.nike.iser.connectedproducts
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class OutsideActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_outside)
+    }
+}
