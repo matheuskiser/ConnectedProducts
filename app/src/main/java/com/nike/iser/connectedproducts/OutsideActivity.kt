241d6abbfa60ffc0abb0950cb77ba89e52b0fe69
Matheus Iser <matheus.iser@nike.com>
2018-10-06 14:16:29 -0700

    Navigating to OutsideActvity which has its own navigation graph file

--- app/src/main/java/com/nike/iser/connectedproducts/OutsideActivity.kt
+++ app/src/main/java/com/nike/iser/connectedproducts/OutsideActivity.kt
@@ -2,11 +2,18 @@ package com.nike.iser.connectedproducts
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class OutsideActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_outside)
+
+        NavigationUI.setupActionBarWithNavController(this, findNavController(R.id.outside_activity_nav_host_fragment))
     }
+
+    override fun onSupportNavigateUp()
+            = findNavController(R.id.outside_activity_nav_host_fragment).navigateUp()
 }
